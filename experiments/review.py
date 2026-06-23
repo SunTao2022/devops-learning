1059438ab079d056b895b371fd115e7282759b94
tao sun <suntaoca2022@gmail.com>
2026-06-23 16:10:29 -0300

    review.py

---
+++ experiments/review.py
@@ -0,0 +1,28 @@
+import argparse
+import subprocess
+from tabulate import tabulate
+
+parser = argparse.ArgumentParser(description="output review")
+parser.add_argument("--cmd" , help="cmd to be used")
+parser.add_argument("--output" , help="exporting")
+parser.add_argument("--verbose" , action="store_true" , help="verbose enable")
+
+argus = parser.parse_args()
+
+
+result = subprocess.run(argus.cmd , shell= True , capture_output=True , text= True)
+print(result.stdout)
+lines = result.stdout.strip().split("\n")
+data = [[line] for line in lines]
+table = tabulate(data , headers=["velue"] , floatfmt="grid")
+print(table)
+
+if argus.output:
+    try:
+        with open(argus.output , "w") as f:
+            f.write(table)
+    except FileNotFoundError:
+        print("FileNotFoundError")
+
+    
+            
