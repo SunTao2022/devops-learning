1a1a1de095df0dfae7377a4d27334f023a22d677
tao sun <suntaoca2022@gmail.com>
2026-08-11 20:07:26 -0300

    chore: remove old terraform-leaning (replaced by terraform-rewrite)

--- terraform-leaning/modules/compute/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vm_public_ips" {
-  value = { for k, v in azurerm_public_ip.vm_ip : k => v.ip_address }
-}
-
-output "vm_ids" {
-  value = { for k, v in azurerm_linux_virtual_machine.vm : k => v.id }
-}
